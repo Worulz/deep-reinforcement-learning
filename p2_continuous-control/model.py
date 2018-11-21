3a2b4eb51d8d269b677cedb42a037a90aec68542
worulz <leon.truong1@gmail.com>
2018-11-21 23:51:57 +1100

    updated readme

--- p2_continuous-control/model.py
+++ p2_continuous-control/model.py
@@ -27,8 +27,6 @@ class Actor(nn.Module):
         self.fc1 = nn.Linear(state_size, fc1_units)
         self.fc2 = nn.Linear(fc1_units, fc2_units)
         self.fc3 = nn.Linear(fc2_units, action_size)
-        #self.fc3 = nn.Linear(fc2_units, fc3_units)
-        #self.fc4 = nn.Linear(fc3_units, action_size)
         self.reset_parameters()
 
     def reset_parameters(self):
@@ -40,8 +38,6 @@ class Actor(nn.Module):
         """Build an actor (policy) network that maps states -> actions."""
         x = F.relu(self.fc1(state))
         x = F.relu(self.fc2(x))
-        #x = F.relu(self.fc3(x))
-        #return F.tanh(self.fc4(x))
         return F.tanh(self.fc3(x))
 
 
@@ -66,14 +62,12 @@ class Critic(nn.Module):
         
         self.fc2 = nn.Linear(fcs1_units+action_size, fc2_units)
         self.fc3 = nn.Linear(fc2_units, 1)
-        #self.fc4 = nn.Linear(fc2_units, 1)
         self.reset_parameters()
 
     def reset_parameters(self):
         self.fcs1.weight.data.uniform_(*hidden_init(self.fcs1))
         self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
         self.fc3.weight.data.uniform_(-3e-3, 3e-3)
-        #self.fc4.weight.data.uniform_(-3e-3, 3e-3)
 
     def forward(self, state, action):
         """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
